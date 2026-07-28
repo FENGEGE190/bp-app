c54e500538002d250db46ae0e8a624e6f611cbd6
FENGEGE190 <734681283@qq.com>
2026-07-28 16:10:29 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-const CACHE_NAME = 'bp-app-v3';
+const CACHE_NAME = 'bp-app-v1.2.0';
 const ASSETS = [
+  './',
   'index.html',
-  'manifest.json'
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -28,6 +31,6 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
+    )).then(() => self.clients.claim())
   );
 });
